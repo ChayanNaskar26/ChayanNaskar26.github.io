f52f0ae910f4cfa5967435c897ab34659667b25b
CHAYAN NASKAR <chayannaskar@live.com>
2017-08-12 20:20:38 +0530

    renew

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,24 @@ console.log(windowsize);
 
 if(windowsize > 959){
     $(".small-picture").remove();
+    $(".web").hover(function(){
+    $(".service-pic").show();
+    $(".service-pic").prepend('<img class="img1" src="images/websites.png" class="img-responsive" style="background-repeat:no-repeat"/>');
+    $(".bkg").css("background-color", "#F48FB1");
+    }, function(){
+        $(".img1").remove();
+        $(".bkg").css("background-color", "#FF5722");
+    });
+
+    $(".mob").hover(function(){
+    $(".service-pic").show();
+    $(".service-pic").prepend('<img class="img2" src="images/mobui.png" class="img-responsive" style="background-repeat:no-repeat"/>');
+    $(".bkg").css("background-color", "#F48FB1");
+    }, function(){
+        $(".img2").remove();
+        $(".bkg").css("background-color", "#FF5722");
+});
+    
 }else{
     $(".picture").remove();
     $("img").css("width", "100%")
@@ -10,26 +28,6 @@ if(windowsize > 959){
     $(".description").css("margin-top", "40px");
     $(".small-picture").css("display", "block");
     $(".small-picture").css("margin", "auto");
-    $(".web").css("margin", "10px 170px");
-    $(".mob").css("margin", "10px 150px");
-    $(".mob").css("float", "left");
     
 }
 
-$(".web").hover(function(){
-    $(".service-pic").show();
-    $(".service-pic").prepend('<img class="img1" src="images/websites.png" class="img-responsive" style="background-repeat:no-repeat"/>');
-    $(".bkg").css("background-color", "#F48FB1");
-}, function(){
-    $(".img1").remove();
-    $(".bkg").css("background-color", "#FF5722");
-});
-
-$(".mob").hover(function(){
-    $(".service-pic").show();
-    $(".service-pic").prepend('<img class="img2" src="images/mobui.png" class="img-responsive" style="background-repeat:no-repeat"/>');
-    $(".bkg").css("background-color", "#F48FB1");
-}, function(){
-    $(".img2").remove();
-    $(".bkg").css("background-color", "#FF5722");
-});
