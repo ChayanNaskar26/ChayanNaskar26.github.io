81fa068a47e2c34e9f60ff8129fbce80a4d5137d
chayannaskar <chayannaskar@live.com>
2019-06-05 20:44:22 +0530

    Added new design

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$.scrollify({
+    section : ".fullpage",
+    sectionName : "section-name",
+    easing: "easeOutExpo",
+    scrollSpeed: 1000,
+    offset : 0,
+    scrollbars: false,
+    updateHash: true,
+    before:function(index, context) {
+        $('.side-nav ul .active').removeClass('active');
+        $('.side-nav ul li:nth-child(' + (index + 1) + ')').addClass('active');
+    }
+});
+
+let goToAbout = () => {
+    window.location.replace("about.html");
+}
+
+let goToUnderCons = () => {
+    window.location.replace("underconstruction.html");
+}
